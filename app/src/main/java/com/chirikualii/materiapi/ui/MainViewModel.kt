17a0cd6b8b0ae4df6ace5b053a59beee8b681341
ringgu <ringguidfa@gmail.com>
2022-10-30 14:21:29 +0700

    membuat MainViewModel

---
+++ app/src/main/java/com/chirikualii/materiapi/ui/MainViewModel.kt
@@ -0,0 +1,29 @@
+package com.chirikualii.materiapi.ui
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.chirikualii.materiapi.data.model.Movie
+import com.chirikualii.materiapi.data.repository.MovieRepo
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class MainViewModel  (val repo: MovieRepo): ViewModel(){
+    private val _listMovie = MutableLiveData<List<Movie>>()
+    val listMovie :LiveData<List<Movie>> =_listMovie
+
+
+    fun doGetPopularMovie(){
+        viewModelScope.launch(Dispatchers.IO){
+           val listData = repo.getPopularMovie()
+
+            withContext(Dispatchers.Main){
+                _listMovie.value = listData
+
+
+            }
+        }
+    }
+}
